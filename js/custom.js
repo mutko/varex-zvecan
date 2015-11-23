2649b8aaaacddd953a652fdebf6706e224824f58
unknown <branko028@yahoo.com>
2015-11-23 19:32:55 +0100

    Smothscrolling added

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,19 @@
 $(function() {
+
+    // Use smooth scrooling when clicking on navigation (from https://css-tricks.com/snippets/jquery/smooth-scrolling/)
+    $('.navbar a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+    
   // blueimp-gallery za ograde
   document.getElementById('ograde').onclick = function (event) {
       event = event || window.event;
