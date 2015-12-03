bef70708329be58638d9da41824b56e870205bdd
unknown <branko028@yahoo.com>
2015-12-03 17:45:51 +0100

    Nove fotografije

--- js/custom.js
+++ js/custom.js
@@ -14,15 +14,6 @@ $(function() {
       }
     });
     
-  // blueimp-gallery za ograde
-  document.getElementById('ograde').onclick = function (event) {
-      event = event || window.event;
-      var target = event.target || event.srcElement,
-          link = target.src ? target.parentNode : target,
-          options = {index: link, event: event},
-          ograde = this.getElementsByTagName('a');
-      blueimp.Gallery(ograde, options);
-  };
   // blueimp-gallery za kapije
   document.getElementById('kapije').onclick = function (event) {
       event = event || window.event;
@@ -32,6 +23,17 @@ $(function() {
           kapije = this.getElementsByTagName('a');
       blueimp.Gallery(kapije, options);
   };
+
+  // blueimp-gallery za ograde
+  document.getElementById('ograde').onclick = function (event) {
+      event = event || window.event;
+      var target = event.target || event.srcElement,
+          link = target.src ? target.parentNode : target,
+          options = {index: link, event: event},
+          ograde = this.getElementsByTagName('a');
+      blueimp.Gallery(ograde, options);
+  };
+  
   // blueimp-gallery za namestaj
   document.getElementById('namestaj').onclick = function (event) {
       event = event || window.event;
@@ -41,6 +43,17 @@ $(function() {
           namestaj = this.getElementsByTagName('a');
       blueimp.Gallery(namestaj, options);
   };
+
+  // blueimp-gallery za stepenista
+  document.getElementById('stepenista').onclick = function (event) {
+      event = event || window.event;
+      var target = event.target || event.srcElement,
+          link = target.src ? target.parentNode : target,
+          options = {index: link, event: event},
+          stepenista = this.getElementsByTagName('a');
+      blueimp.Gallery(stepenista, options);
+  };
+  
   // blueimp-gallery za felna 1
   document.getElementById('felna_1').onclick = function (event) {
       event = event || window.event;
@@ -50,6 +63,7 @@ $(function() {
           felna_1 = this.getElementsByTagName('a');
       blueimp.Gallery(felna_1, options);
   };
+  
   // blueimp-gallery za felna 2
   document.getElementById('felna_2').onclick = function (event) {
       event = event || window.event;
@@ -59,6 +73,7 @@ $(function() {
           felna_2 = this.getElementsByTagName('a');
       blueimp.Gallery(felna_2, options);
   };
+  
   // blueimp-gallery za felna 3
   document.getElementById('felna_3').onclick = function (event) {
       event = event || window.event;
@@ -68,6 +83,7 @@ $(function() {
           felna_3 = this.getElementsByTagName('a');
       blueimp.Gallery(felna_3, options);
   };
+  
   // blueimp-gallery za felna 4
   document.getElementById('felna_4').onclick = function (event) {
       event = event || window.event;
@@ -77,4 +93,25 @@ $(function() {
           felna_4 = this.getElementsByTagName('a');
       blueimp.Gallery(felna_4, options);
   };
+  
+  // blueimp-gallery za felna 5
+  document.getElementById('felna_5').onclick = function (event) {
+      event = event || window.event;
+      var target = event.target || event.srcElement,
+          link = target.src ? target.parentNode : target,
+          options = {index: link, event: event},
+          felna_5 = this.getElementsByTagName('a');
+      blueimp.Gallery(felna_5, options);
+  };
+
+  // blueimp-gallery za rolbar
+  document.getElementById('rolbar').onclick = function (event) {
+      event = event || window.event;
+      var target = event.target || event.srcElement,
+          link = target.src ? target.parentNode : target,
+          options = {index: link, event: event},
+          rolbar = this.getElementsByTagName('a');
+      blueimp.Gallery(rolbar, options);
+  };
+  
 });
